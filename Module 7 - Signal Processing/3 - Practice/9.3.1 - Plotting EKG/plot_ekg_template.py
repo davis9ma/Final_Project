7cefec939db9ad26ea26a264eb0fd8729e389e4f
davis9ma <davis9ma@dukes.jmu.edu>
2024-10-29 11:47:14 -0400

    All Changes to Date

--- Module 7 - Signal Processing/3 - Practice/9.3.1 - Plotting EKG/plot_ekg_template.py
+++ Module 7 - Signal Processing/3 - Practice/9.3.1 - Plotting EKG/plot_ekg_template.py
@@ -1,18 +1,24 @@
 
 import matplotlib.pyplot as plt
 import numpy as np
+from matplotlib import matplotlib_fname
+
 
 # import the CSV file using numpy
-path = '../../../data/ekg/mitdb_201.csv'
+path = "C:\\Users\\Macon Davis\\Documents\\GitHub\\engr340-fall2024\\data\\ekg\\mitdb_100.csv"
 
 # load data in matrix from CSV file; skip first two rows
+matrix = np.loadtxt(path,delimiter=",",skiprows=2)
 
-### Your code here ###
 
 # save each vector as own variable
 
-### Your code here ###
+time = matrix[:,0]
+mlii = matrix[:,1]
+v5 = matrix[:,2]
 
 # use matplot lib to generate a single
 
-### Your code here ###
+plt.title("EKG Data")
+plt.plot(time,v5)
+plt.show()
